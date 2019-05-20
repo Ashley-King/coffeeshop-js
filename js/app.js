fdd0fcad09fd19d9f41bf7f3b57e4a7033924813
Ashley King <ashleyot@gmail.com>
2019-05-20 19:29:08 -0400

    toggle nav bar

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@
 window.addEventListener("load", () => {
   document.querySelector(".preloader").style.display = "none";
 });
+
+//nav btn
+document.querySelector(".navBtn").addEventListener("click", () => {
+  document.querySelector(".nav").classList.toggle("nav--show");
+});
