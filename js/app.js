1be845976a4b4bb085ba105da5032a0b8ec25efb
Ashley King <ashleyot@gmail.com>
2019-05-20 19:37:05 -0400

    refactored js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 //hide preloader when all scripts, links, images have loaded
-//window even listener
-window.addEventListener("load", () => {
-  document.querySelector(".preloader").style.display = "none";
-});
+const eventListeners = () => {
+  const ui = new UI();
+  //window eventlistener
+  window.addEventListener("load", () => {
+    ui.hidePreloader();
+  });
 
-//nav btn
-document.querySelector(".navBtn").addEventListener("click", () => {
+  //nav btn
+  document.querySelector(".navBtn").addEventListener("click", () => {
+    ui.toggleNav();
+  });
+};
+
+function UI() {}
+
+UI.prototype.hidePreloader = () => {
+  document.querySelector(".preloader").style.display = "none";
+};
+
+UI.prototype.toggleNav = () => {
   document.querySelector(".nav").classList.toggle("nav--show");
-});
+};
+
+eventListeners();
