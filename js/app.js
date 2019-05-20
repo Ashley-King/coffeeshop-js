ce1f4f72babc41d3ad7a8adaa481651ab8a88aaf
Ashley King <ashleyot@gmail.com>
2019-05-20 19:18:40 -0400

    preloader hidden once page loads

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//hide preloader when all scripts, links, images have loaded
+//window even listener
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").style.display = "none";
+});
