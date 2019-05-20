634c84d2088109df539201704107bf24c4cc698b
Ashley King <ashleyot@gmail.com>
2019-05-20 19:53:54 -0400

    play/pause header video

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
 //hide preloader when all scripts, links, images have loaded
+//constructor function
+class UI {
+  constructor() {}
+}
+
+UI.prototype.hidePreloader = () => {
+  document.querySelector(".preloader").style.display = "none";
+};
+
+UI.prototype.toggleNav = () => {
+  document.querySelector(".nav").classList.toggle("nav--show");
+};
+
+UI.prototype.videoControls = () => {
+  let btn = document.querySelector(".video__switch-btn");
+  if (!btn.classList.contains("btnSlide")) {
+    btn.classList.add("btnSlide");
+    document.querySelector(".video__item").pause();
+  } else {
+    btn.classList.remove("btnSlide");
+    document.querySelector(".video__item").play();
+  }
+};
+
 const eventListeners = () => {
   const ui = new UI();
   //window eventlistener
@@ -10,16 +34,11 @@ const eventListeners = () => {
   document.querySelector(".navBtn").addEventListener("click", () => {
     ui.toggleNav();
   });
-};
 
-function UI() {}
-
-UI.prototype.hidePreloader = () => {
-  document.querySelector(".preloader").style.display = "none";
-};
-
-UI.prototype.toggleNav = () => {
-  document.querySelector(".nav").classList.toggle("nav--show");
+  //control video
+  document.querySelector(".video__switch").addEventListener("click", () => {
+    ui.videoControls();
+  });
 };
 
 eventListeners();
